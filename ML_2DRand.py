5e60f3e73a2a99d3adcbbdcebaf8fabbc8a9c7b2
Dilraj <dilraj@pop-os.localdomain>
2021-10-14 22:30:00 -0700

    moving stuff around

---
+++ ML_2DRand.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from DataSetGen.DataGen_2DRand import *
+
