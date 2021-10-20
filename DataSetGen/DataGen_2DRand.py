8d7da0c60fdb5642030dfc3ff5fc866952640c61
Dilraj Ghuman <dilraj.ghuman96@gmail.com>
2021-10-20 16:24:08 -0700

    small changes and restructuring. Data conversion into tensors

--- DataSetGen/DataGen_2DRand.py
+++ DataSetGen/DataGen_2DRand.py
@@ -1,67 +1,74 @@
-# 2021-Oct-10 Dilraj Ghuman 
+# 2021-Oct-10 Dilraj Ghuman
 # This is a file to generate some simple 2D data to train on as practice
 
 import random as rand
 import numpy as np
 import pickle
 
-# Data Structure to save the numpy array and direction
-data_set = {
-    "slope": [],
-    "intercept": [],
-    "arr": [],
-}
-
 def save_pickle(obj, filename):
-    with open(filename, 'wb') as output: # Overwrites 
+    with open(filename, "wb") as output:  # Overwrites
         pickle.dump(obj, output, -1)
     return 0
 
-def load_pickle(obj, filename):
-    with open(filename, 'rb') as input:
-        obj = pickle.load(input)
-    return 0
+
+def load_pickle(filename):
+    with open(filename, "rb") as input:
+        return pickle.load(input)
+
 
 def make_track(theta, intercept):
     # Angle to slope
     def slope():
         rise = np.sin(theta)
         run = np.cos(theta)
-        return rise/run
+        return rise / run
 
     m = slope()
     # Feed track a numpy array, it'll spit out the y_values
     def track(x):
-        y = m*x + intercept
+        y = m * x + intercept
         return y
 
     return track, m
 
+
 # Need to compute intersections of grid with function. Suppose the grid is passed as numpy array of zeros. Size determines granularity in a 32 x 32 (pixel) setting. So, a 32 x 32 array has each entry represent a pixel. Return array filled with hit info.
 def hit_array(empty_array, _track):
-    dim = empty_array.shape[0]                                             # Read in dimension
-    bin_array = np.linspace(0,32,dim+1)                                    # Get bin ranges that need to be checked
+    dim = empty_array.shape[0]  # Read in dimension
+    bin_array = np.linspace(0, 32, dim + 1)  # Get bin ranges that need to be checked
     for n in range(dim):
-        xval = np.linspace(bin_array[n], bin_array[n+1], 100)              # Make domain to check
-        yval = _track(xval)                                                # Compute values over domain
-        for m in range(dim):                                               # Loop over values y bins
-            cond = np.logical_and(np.less_equal(yval, bin_array[m+1]),np.greater_equal(yval, bin_array[m]))    # Check if yval is in bin
-            if cond.any():                                                                                  # If it is in bin, fill the bin
-                empty_array[m,n] = 1
+        xval = np.linspace(bin_array[n], bin_array[n + 1], 100)  # Make domain to check
+        yval = _track(xval)  # Compute values over domain
+        for m in range(dim):  # Loop over values y bins
+            cond = np.logical_and(
+                np.less_equal(yval, bin_array[m + 1]),
+                np.greater_equal(yval, bin_array[m]),
+            )  # Check if yval is in bin
+            if cond.any():  # If it is in bin, fill the bin
+                empty_array[m, n] = 1
     return 0
 
+
 def main():
-    hits = np.zeros([32,32])
-    for i in range(100):                        # Make 100 entries
+    # Data Structure to save the numpy array and direction
+    data_set = {
+        "labels": [],
+        "arr": [],
+    }
+    for i in range(100):  # Make 100 entries
+        hits = np.zeros([32, 32])
         b = rand.uniform(0, 32)
-        angle = rand.uniform(0, 2*np.pi)
+        angle = rand.uniform(0, 2 * np.pi)
         track, m = make_track(angle, b)
         hit_array(hits, track)
-        data_set["slope"].append(m)
-        data_set["intercept"].append(b)
+        data_set["labels"].append(
+            np.array(([b, m]))
+        )  # Appended as intercept then slope
         data_set["arr"].append(hits)
-    save_pickle(data_set, '/home/users/ghuman/ML/DataSetGen/2D_data/data_1.pkl')
+    save_pickle(
+        data_set, "/home/dilraj/Documents/ML-testing/DataSetGen/2D_data/data_1.pkl"
+    )
+
 
 if __name__ == "__main__":
     main()
-              
