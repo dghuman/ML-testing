f794b9d2046336a127b211f62d31ff6ed5934ddc
Dilraj Ghuman <dilraj.ghuman96@gmail.com>
2021-10-25 14:25:12 -0700

    small update

--- ML_2DRand.py
+++ ML_2DRand.py
@@ -15,12 +15,25 @@ First load in the data that will be used to train. Dictionary is loaded into dat
 '''
 data = load_pickle("/home/dilraj/Documents/ML-testing/DataSetGen/2D_data/data_1.pkl")
 
+new_arr = []
+for arr in data["arr"]:
+    new_arr.append(arr.flatten())
+
 train_labels = tf.convert_to_tensor(np.array((data["labels"])), dtype=tf.float32)
-train_data = tf.convert_to_tensor(np.array((data["arr"])), dtype=tf.float32)
+train_data = tf.convert_to_tensor(np.array((new_arr)), dtype=tf.float32)
 
 dataset = tf.data.Dataset.from_tensor_slices((train_data, train_labels))
 
+'''
+Will use a simple dense NN at first (dense meaning that the layers are completely connected, so every node is connected to every other node). Sequential model does this well:
+'''
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(1024,)), 
+    tf.keras.layers.Dense(10, activation=tf.nn.relu),
+    tf.keras.layers.Dense(2)
+])
+
+features, labels = next(iter(dataset))
 
 
-
-# Will use a simple dense NN at first (dense meaning that the layers are completely connected, so every node is connected to every other node). This means a call to the simple layer.
