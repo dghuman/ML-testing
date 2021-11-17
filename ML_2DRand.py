732fad2915854b34dd2d5df2012497549132555c
Dilraj Ghuman <dilraj.ghuman96@gmail.com>
2021-11-16 17:44:25 -0800

    Adding parser to training script.

--- ML_2DRand.py
+++ ML_2DRand.py
@@ -10,20 +10,64 @@ from tensorflow.keras import layers
 from DataSetGen.DataGen_2DRand import load_pickle, save_pickle
 from tools.timer import Timer  # load in Timer class
 
+usage = "usage: %prog [options] inputfile"
+parser = OptionParser(usage)
 timer = Timer()
-
-"""
-Arg parser should be used here
-"""
-
+parser.add_option(
+    "-din",
+    "--datain",
+    type="string",
+    default="/home/dilraj/Documents/ML-testing/DataSetGen/2D_data/data_1.pkl",
+    dest="DATA_IN",
+    help="Training data input file in pickle format (.pkl).",
+)
+parser.add_option(
+    "-tdin",
+    "--testdata",
+    type="string",
+    default="/home/dilraj/Documents/ML-testing/DataSetGen/2D_data/data_2.pkl",
+    dest="TEST_IN",
+    help="Test data input file in pickle format (.pkl).",
+)
+parser.add_option(
+    "-l",
+    "--learn",
+    type="bool",
+    default=False,
+    dest="LEARN",
+    help="Boolian to determine if this is training that is continuing rather than starting anew.",
+)
+parser.add_option(
+    "-mf",
+    "--modelfile",
+    type="string",
+    default="/home/dilraj/Documents/ML-testing/fit_models/model_test.tf",
+    dest="MODEL_OUT",
+    help="Output model file saved in the tensorflow file type (.tf).",
+)
+parser.add_option(
+    "-t",
+    "--timer",
+    type="bool",
+    default=True,
+    dest="TIME",
+    help="Timer option. Set to True by default and times how long current training has been running.",
+)
+parser.add_option(
+    "-h",
+    "--history",
+    type="string",
+    default="/home/dilraj/Documents/ML-testing/fit_models/model_test_history.pkl",
+    dest="HISTORY",
+    help="History file containing information about the training stats saved in pickled format (.pkl).",
+)
+(options, args) = parser.parse_args()
 
 """ 
 First load in the data that will be used to train. Dictionary is loaded into data. Calls are 'arr' and 'labels'. The loaded data is then converted into tensors so that they can be zipped into a tensorflow data set that is parsable by the workflow.
 """
-data = load_pickle("/home/dilraj/Documents/ML-testing/DataSetGen/2D_data/data_1.pkl")
-test_data = load_pickle(
-    "/home/dilraj/Documents/ML-testing/DataSetGen/2D_data/data_2.pkl"
-)
+data = load_pickle(options.DATA_IN)
+test_data = load_pickle(options.TEST_IN)
 
 new_arr = []
 new_arr_t = []
@@ -48,28 +92,30 @@ test_dataset = test_dataset.batch(10)
 """
 Will use a simple dense NN at first (dense meaning that the layers are completely connected, so every node is connected to every other node). Sequential model does this well:
 """
-
-model = tf.keras.Sequential(
-    [
-        tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(1024,)),
-        tf.keras.layers.Dense(10, activation=tf.nn.relu),
-        tf.keras.layers.Dense(2),
-    ]
-)
+if options.LEARN:
+    model = tf.keras.models.load_model(options.MODEL_OUT)
+else:
+    model = tf.keras.Sequential(
+        [
+            tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(1024,)),
+            tf.keras.layers.Dense(10, activation=tf.nn.relu),
+            tf.keras.layers.Dense(2),
+        ]
+    )
 
 loss_object = tf.keras.losses.MeanSquaredError()
 optimizer = tf.keras.optimizers.RMSprop()
 
 model.compile(optimizer=optimizer, loss=loss_object, metrics=["accuracy"])
 
-timer.start()
+if options.TIMER:
+    timer.start()
 History = model.fit(dataset, epochs=20000, verbose=1)
-timer.stop()
+if options.TIMER:
+    timer.stop()
 
 history = {}
 history = History.history
 
-model.save("/home/dilraj/Documents/ML-testing/fit_models/model_test.tf")
-save_pickle(
-    history, "/home/dilraj/Documents/ML-testing/fit_models/model_test_history.pkl"
-)
+model.save(options.MODEL_OUT)
+save_pickle(history, options.HISTORY)
